573f46e44086929a960867abaa88a77ea39a3498
Eric Nelson <eric.nelson@boundarydevices.com>
2011-03-04 17:57:28 -0700

    rfid: support message queue and transactional responses

--- rfid.cpp
+++ rfid.cpp
@@ -8,13 +8,26 @@
 #include "rfid.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <assert.h>
+#include <QMap>
+
+rfid_message_t::rfid_message_t(int code, void const *data, unsigned len )
+	: code_(code)
+	, data_((char const *)data,len)
+{
+}
+
+rfid_message_t::rfid_message_t(int code, QByteArray const &data)
+	: code_(code)
+	, data_(data)
+{
+}
 
 rfidReader_t::rfidReader_t()
 	: notifier(0)
 	, sldev(0)
 	, waiting(false)
 	, ispresent(false)
-	, timer(this)
 {
 	char const *devname = getenv("RFIDDEV");
 	if (devname) {
@@ -22,9 +35,7 @@ rfidReader_t::rfidReader_t()
 		if (sldev->isOpen()) {
                         notifier = new QSocketNotifier(sldev->getFd(),notifier->Read);
 			QObject::connect(notifier, SIGNAL(activated(int)), this, SLOT(readData(int)));
-			connect(&timer, SIGNAL(timeout()), this, SLOT(timeout()));
-			timer.setInterval(100);
-			timer.start();
+			startTimer(100);
 		} else {
 			delete sldev ; sldev = 0 ;
 		}
@@ -33,7 +44,6 @@ rfidReader_t::rfidReader_t()
 
 rfidReader_t::~rfidReader_t()
 {
-	timer.stop();
 	if (notifier) {
 		delete notifier ;
 		notifier = 0 ;
@@ -49,6 +59,18 @@ bool rfidReader_t::isPresent(void) const {
 	return sldev && sldev->serialNumber(serial);
 }
 
+bool rfidReader_t::isBusy(void) const {
+	return waiting ;
+}
+
+int rfidReader_t::getElapsed(void) const {
+	return wait_time.elapsed();
+}
+
+int rfidReader_t::getTicks(void) const {
+	return ticks_ ;
+}
+
 static QString const cardTypeNames[] = {
    QString("None")
 ,  QString("Mifare Standard 1K card")
@@ -60,6 +82,42 @@ static QString const cardTypeNames[] = {
 };
 
 static unsigned const numCardTypeNames = sizeof(cardTypeNames)/sizeof(cardTypeNames[0]);
+static QString const resultName("result");
+static QString const dataName("data");
+static QString const codeName("code");
+
+static QString const empty_string ;
+static char const hexDigits[17] = "0123456789ABCDEF";
+static QString encode(void const *data, unsigned len)
+{
+	if (0 == len)
+		return empty_string ;
+	QString s ; s.reserve(3*len+1);
+	char const *bytes = (char const *)data;
+	while (len--) {
+		char const c = *bytes++ ;
+		if (isalnum(c)) {
+			s += c ;
+		} else {
+		    s += '%';
+		    s += hexDigits[c>>4];
+		    s += hexDigits[c&0x0f];
+		}
+	}
+	return s ;
+}
+
+static void build_response( rfid_message_list_t const &msgs,
+			    QVariantList 	      &vl)
+{
+        rfid_message_list_t::const_iterator msgit ;
+	for (msgit = msgs.begin(); msgit != msgs.end(); msgit++) {
+                QMap<QString, QVariant> map ;
+		map[resultName] = QVariant((*msgit).code_);
+		map[dataName] = QVariant(encode((*msgit).data_.constData(),(*msgit).data_.length()));
+		vl.push_back(QVariant(map));
+	}
+}
 
 void rfidReader_t::readData(int fd)
 {
@@ -68,10 +126,27 @@ void rfidReader_t::readData(int fd)
 		void const *rxdata ;
 		unsigned rxlen ;
 		void *context ;
-		if (sldev->receive(resp,rxdata,rxlen,context)) {
+		while (sldev->receive(resp,rxdata,rxlen,context)) {
 			waiting = false ;
 			long long serial ;
 			bool nowPresent = sldev->serialNumber(serial);
+			if (context) {
+				assert (!requests_.empty());
+				assert (!responses_.empty());
+				rfid_message_t &r = responses_.back();
+				r.code_ = resp ;
+				r.data_ = QByteArray((char const *)rxdata,rxlen);
+				rfid_message_list_t &trans = requests_.front();
+				if (trans.empty() || (0 != r.code_)) {
+					requests_.pop_front();
+					QVariantList v ;
+					build_response(responses_,v);
+					responses_.clear();
+					emit response(resp,v);
+				} else {
+					send();
+				}
+			}
 			if (nowPresent != ispresent) {
 				if (nowPresent) {
 					char buf[32];
@@ -90,17 +165,79 @@ void rfidReader_t::readData(int fd)
 				ispresent = nowPresent ;
 			}
 		}
+	} else {
+		printf( "No notifier %p or dev %p\n", notifier, sldev);
 	}
 }
 
-bool rfidReader_t::send(int cmd, QString data)
+bool rfidReader_t::send(int cmd, QByteArray data)
 {
-	if (data.isEmpty()) {
-                return send((stronglink_t::command_e)cmd,0,0,0);
-	} else {
-		QByteArray bytes(data.toAscii());
-		return send((stronglink_t::command_e)cmd,bytes.constData(),bytes.length(),0);
+	rfid_message_t msg(cmd,data);
+	rfid_message_list_t list ; list.push_back(msg);
+	requests_.push_back(list);
+	return send();
+}
+
+static bool build_message_list( QVariantList const &vl,
+			        rfid_message_list_t &msgs)
+{
+	QVariantList::const_iterator it ;
+
+	for (it = vl.begin() ; it != vl.end(); it++) {
+		QVariant const &el = (*it);
+		if (QVariant::Map == el.type()) {
+			QVariantMap v = el.toMap();
+			QVariantMap::const_iterator codeit = v.find(codeName);
+			QVariantMap::const_iterator datait = v.find(dataName);
+			if (codeit != v.end()) {
+				QString data = (datait != v.end())
+						? (*datait).toString()
+					        : empty_string;
+				msgs.push_back(rfid_message_t((*codeit).toInt(),data.toAscii()));
+			} else {
+				printf( "message is missing code\n");
+				return false ;
+			}
+		} else {
+			printf( "Invalid message: use form: { code:0x01 [, data:'\x01\x02']}\n");
+			return false ;
+		}
 	}
+	return !msgs.empty();
+}
+
+bool rfidReader_t::send(QVariantList requests)
+{
+	rfid_message_list_t msgs ;
+	if (build_message_list(requests,msgs)) {
+		requests_.push_back(msgs);
+		return send();
+	} else
+		printf ("Error decoding message list\n");
+	return false ;
+}
+
+bool rfidReader_t::send()
+{
+	if (sldev && !waiting) {
+		if (!requests_.empty()) {
+                        rfid_message_list_t &list = requests_.front();
+			if (!list.empty()) {
+				rfid_message_t &msg = list.front();
+                                if (send((stronglink_t::command_e)msg.code_, msg.data_.constData(), msg.data_.length(), &msg )) {
+					responses_.push_back(list.front());
+					list.pop_front();
+				}
+				else
+					printf( "send error\n");
+			}
+			else
+				printf("empty list in send\n");
+		}
+		else
+			printf("Nothing to send\n");
+	}
+	return false ;
 }
 
 bool rfidReader_t::send(stronglink_t::command_e cmd, void const *data, unsigned len, void *context)
@@ -108,22 +245,28 @@ bool rfidReader_t::send(stronglink_t::command_e cmd, void const *data, unsigned 
 	bool worked = false ;
 	if (sldev && !waiting) {
 		worked = waiting = sldev->send(cmd,data,len,context);
-		if (waiting)
-			wait_time.restart();
+		if (waiting) {
+                        wait_time.restart();
+		} else
+			printf( "sldev send error\n");
 	} else {
 		fprintf(stderr, "Attempt to send on busy device\n");
 	}
 	return worked ;
 }
 
-static QString const empty_string ;
-
-void rfidReader_t::timeout(void)
+void rfidReader_t::timerEvent(QTimerEvent *)
 {
 	if (waiting && (2000 < wait_time.elapsed())) {
 		printf( "timeout waiting for response\n");
 		waiting = false ;
 	}
-	if (!waiting)
-	    send(sldev->SL_SELECTCARD,0,0,this);
+	if (!waiting){
+		if (requests_.empty()) {
+			send(sldev->SL_SELECTCARD,0,0,0);
+		} else {
+			send();
+		}
+	}
+	++ticks_ ;
 }
