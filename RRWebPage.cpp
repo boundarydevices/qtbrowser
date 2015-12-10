13ad35e69f7f8726e04b6608e81623b8fa36fb8f
Ian Coolidge <ian@boundarydevices.com>
2015-12-10 12:22:50 -0800

    RRWebPage.cpp: Remove comments

--- RRWebPage.cpp
+++ RRWebPage.cpp
@@ -17,20 +17,4 @@ void RRWebPage::javaScriptConsoleMessage( const QString & message, int lineNumbe
 {
 //do something!
 qDebug() << message << lineNumber << sourceID;
-}@
-
-Here is where I assign an instance of RRWebPage in html5applicationviewer.cpp:
-
-@NavigationControllerPrivate::NavigationControllerPrivate(QWidget *parent, QGraphicsWebView *webView)
-: m_webPage(0)
-, m_webWidget(0)
-, m_graphicsWebView(webView)
-, m_webNavigation(0)
-{
-Q_UNUSED(parent);
-m_graphicsWebView->setAcceptTouchEvents(true);
-m_webPage = new RRWebPage;
-m_graphicsWebView->setPage(m_webPage);
-m_webNavigation = new WebNavigation(m_graphicsWebView, m_webPage);
-m_webNavigation->setParent(m_graphicsWebView);
 }
